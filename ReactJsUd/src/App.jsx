84b754d67eb0aa01111bb86896811a1e7ce48654
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-01-20 17:53:46 -0300

    state com arrays e objetos

--- ReactJsUd/src/App.jsx
+++ ReactJsUd/src/App.jsx
@@ -1,42 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component{
       // this.handlePclick = this.handlePclick.bind(this);
         state = {
-        nome: 'Gabriel Santos',
-        counter: 0
+          posts: [
+            {
+              id:1,
+              title:'o titulo 1',
+              body: 'O corpo 1'
+            },
+            {
+              id:2,
+              title:'o titulo 2',
+              body: 'O corpo 2'
+            },
+            {
+              id:3,
+              title:'o titulo 3',
+              body: 'O corpo 3'
+            }
+          ]
       };
-    handlePclick = () => {
-      this.setState({nome : 'Batemarque'});
-    }
-    handleAClick = (event) => {
-      event.preventDefault();
-      const {counter} = this.state;
-      this.setState({counter : counter + 1 });
-    }
 
 
   render(){
-      const {nome, counter} = this.state;
+      const { posts } = this.state;
     return (
           <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p onClick={this.handlePclick}>
-               {nome} {counter}
-              </p>
-              <a       
-                onClick={this.handleAClick}     
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Este é o link
-              </a>
-            </header>
+            {posts.map((post) => { return(
+              <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            </div>
+            )})}
           </div>
         );
     }

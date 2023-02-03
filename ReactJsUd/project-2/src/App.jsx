30b7881584b178d437ab22a72b13afd03be903b2
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-02-03 16:06:08 -0300

    hook useCallBack

--- ReactJsUd/project-2/src/App.jsx
+++ ReactJsUd/project-2/src/App.jsx
@@ -1,45 +1,31 @@
+import P from 'prop-types';
 import './App.css';
-import { useState,useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-const eventfn = () => { 
-  console.log('h1 clickado')
-}
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('Filho, renderizou');
+  return <button onClick={() => incrementButton(100)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
+};
 
 function App() {
-    const [counter, setCounter] = useState(0);
-    const [counter2, setCounter2] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-// componentDidUpdate - executa toda vez que o componente atualiza
-  // useEffect(() => {
-  //   console.log('componentDidUpdate');
-  // });
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
+  }, []);
 
-//componentDidMount - executa 1x
-  useEffect(() => {
-    console.log('componentDidMount');
-}, []);
-
-//componentWillAmount - limpeza quando desmontar o componente
-useEffect(() => {
-  document.querySelector('h1')?.addEventListener('click', eventfn);
-  return () => {
-    document.querySelector('h1')?.removeEventListener('click', eventfn);
-  }
-}, []);
-
-
-//com dependencia - executa toda vez que a dependencia mudar
-  useEffect(() => {
-  console.log('c1', counter, 'c2', counter2);
-}, [counter, counter2]);
-
+  console.log('Pai, renderizou');
 
   return (
-      <div className="App">
-        <h1>contador1: {counter}, contador2: {counter2}</h1>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
-        <button onClick={() => setCounter2(counter2 + 1)}>+</button>
-     </div>   
+    <div className="App">
+      <p>Teste 3</p>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
+    </div>
   );
 }
 

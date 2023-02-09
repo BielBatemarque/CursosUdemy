c46cbfa81a1c126b0713c6da3a580c08bd5937fb
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-02-09 02:35:19 -0300

    hook useReducer

--- ReactJsUd/project-2/src/App.jsx
+++ ReactJsUd/project-2/src/App.jsx
@@ -1,14 +1,39 @@
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import {Div} from './components/Div';
+
+const globalState = {
+  title: 'O titulo que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'muda':
+      console.log('chamou muda com ', action.payload);
+      return {...state, title: action.payload};
+    case 'inverter':
+      console.log('chamou inverter');
+      const { title }= state;
+      return {...state, title: title.split('').reverse().join('')};
+  }
+
+  console.log('NENHUMA ACTION ENCONTRADA');
+  return {...state};
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const {counter, title, body} = state;
 
   return(
-    <AppContext>
-      <Div />
-    </AppContext>
-  )
+    <div>
+      <h1>{title} {counter}</h1>
+      <button onClick={() => dispatch({ type: 'muda', payload: new Date().toLocaleString('pt-br')}) }>Muda</button>
+      <button onClick={() => dispatch({ type: 'inverter' }) }>inverter</button>
+      <button onClick={() => dispatch({ type: 'qualquer coisa'}) }>chama</button>
+    </div>
+  );
 }
 
 export default App;
--- ReactJsUd/project-2/src/contexts/AppContext/data.js
+++
@@ -1,5 +0,0 @@
-export const globalState ={
-    title: 'O titulo que contexto',
-    body: 'O body do contexto',
-    counter: 0,
-  };

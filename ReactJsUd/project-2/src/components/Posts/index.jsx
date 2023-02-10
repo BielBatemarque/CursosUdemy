c4ba1ed53e7d83d7b02d0c0b765cd7f6bdd1893d
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-02-10 12:17:20 -0300

    Organização com useEfect, useReducer, useContext, useRef

--- ReactJsUd/project-2/src/components/Posts/index.jsx
+++ ReactJsUd/project-2/src/components/Posts/index.jsx
@@ -1,12 +1,23 @@
 import { useContext, useEffect, useRef } from 'react';
+import {
+  decrementCounter,
+  incrementCounter,
+} from '../../contexts/CounterProvider/action';
+import { CounterContext } from '../../contexts/CounterProvider/context';
 import { loadPosts } from '../../contexts/PostsProvider/actions';
 import { PostsContext } from '../../contexts/PostsProvider/context';
 
 export const Posts = () => {
   const isMounted = useRef(true);
+  isMounted.current = true;
+
   const postsContext = useContext(PostsContext);
   const { postsState, postsDispatch } = postsContext;
 
+  const counterContext = useContext(CounterContext);
+  const { counterState, counterDispatch } = counterContext;
+
+  
   useEffect(() => {
     loadPosts(postsDispatch).then((dispatch) => {
       if (isMounted.current) {
@@ -21,6 +32,12 @@ export const Posts = () => {
 
   return (
     <div>
+      <button onClick={() => incrementCounter(counterDispatch)}>
+        Counter {counterState.counter}+
+      </button>
+      <button onClick={() => decrementCounter(counterDispatch)}>
+        Counter {counterState.counter}-
+      </button>
       <h1>POSTS</h1>
       {postsState.loading && (
         <p>
--- ReactJsUd/project-2/src/templates/App.jsx
+++ ReactJsUd/project-2/src/templates/App.jsx
@@ -1,16 +1,19 @@
 
 import { Posts } from '../components/Posts';
+import { CounterProvider } from '../contexts/CounterProvider';
 import { PostsProvider } from '../contexts/PostsProvider';
 import './styles.css';
 
 function App() {
 
   return(
+   <CounterProvider>
     <PostsProvider>
       <div>
       <Posts/>
       </div>
     </PostsProvider>
+   </CounterProvider> 
     );
 }
 

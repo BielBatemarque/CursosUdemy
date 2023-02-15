f22d58a05bc6787f2ffa0b42fa5fc546e79d04ff
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-02-15 17:12:24 -0300

    criando hook useAsync

--- ReactJsUd/project-3/src/templates/home/index.jsx
+++ ReactJsUd/project-3/src/templates/home/index.jsx
@@ -1,42 +1,87 @@
-import { useEffect, useState } from 'react';
-import {useFetch} from '../../hooks/useFetch';
+import { useCallback, useEffect, useState } from 'react';
 
-const Home = () => {
-  const [postId, setPostId] = useState('');
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
-    headers: {
-      abc: '1' + postId,
-    }
+const useAsync = (asyncFunction, shouldRun) => {
+  const [state, setState] = useState({
+    result: null,
+    error: null,
+    status: 'idle',
   });
 
-    useEffect(() => {
-      console.log('ID do post', postId);
-    }, [postId]);
+  const run = useCallback(() => {
+    setState({
+      result: null,
+      error: null,
+      status: 'pending',
+    });
 
-  //  console.log(result);
-   if(loading){
-    return <p>Loading ...</p>
-   }
-   
-   const handleClick = (id) => {
-     setPostId(id);
-   }
+    return asyncFunction()
+      .then((response) => {
+        setState({
+          result: response,
+          error: null,
+          status: 'settled',
+        });
+      })
+      .catch((err) => {
+        setState({
+          result: null,
+          error: err,
+          status: 'error',
+        });
+      });
+  }, [asyncFunction]);
 
-   if(!loading && result){
-    return <div>
-      {result?.length > 0 ? result.map(p => (
-        <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}> 
-          <p>{p.title}</p>
-        </div>
-      )) : (
-        <div onClick={() => handleClick('')}> 
-          <p>{result.title}</p>
-        </div>
-      )}
-    </div>
-   } 
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [run, shouldRun]);
 
-  return <h1>oi</h1>
-}
+  return [run, state.result, state.error, state.status];
+};
+
+const fetchData = async () => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts/');
+  const json = await data.json();
+  return json;
+};
+
+ const Home = () => {
+  const [posts, setPosts] = useState(null);
+  const [reFetchData, result, error, status] = useAsync(fetchData, true);
+  const [reFetchData2, result2, error2, status2] = useAsync(fetchData, true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      reFetchData();
+    }, 6000);
+  }, [reFetchData]);
+
+  useEffect(() => {
+    console.log(result2);
+  }, [result2]);
+
+  function handleClick() {
+    reFetchData();
+  }
+
+  if (status === 'idle') {
+    return <pre>idle: Nada executando</pre>;
+  }
+
+  if (status === 'pending') {
+    return <pre>pending: Loading...</pre>;
+  }
+
+  if (status === 'error') {
+    return <pre>error: {error.message}</pre>;
+  }
+
+  if (status === 'settled') {
+    return <pre onClick={handleClick}>settled: {JSON.stringify(result, null, 2)}</pre>;
+  }
+
+  return 'IXIII';
+};
 
 export default Home;

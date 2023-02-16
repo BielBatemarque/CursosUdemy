316cd8423a0f59cf20c7b70943d9bff482e63347
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-02-15 22:39:48 -0300

    useLayoutEffect

--- ReactJsUd/project-3/src/templates/home/index.jsx
+++ ReactJsUd/project-3/src/templates/home/index.jsx
@@ -1,87 +1,30 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState, useRef, useLayoutEffect } from 'react';
 
-const useAsync = (asyncFunction, shouldRun) => {
-  const [state, setState] = useState({
-    result: null,
-    error: null,
-    status: 'idle',
+const Home = () => {
+  const [counted, setCounted] = useState([0,1,2,3,4]);
+  const divRef = useRef();
+
+  useLayoutEffect(() => {
+    const now = Date.now();
+    while(Date.now() < now + 600);
+    divRef.current.scrollTop = divRef.current.scrollHeight;
   });
 
-  const run = useCallback(() => {
-    setState({
-      result: null,
-      error: null,
-      status: 'pending',
-    });
+  const handleCLick = () => {
+    setCounted(c => [...c, + c.slice(-1) + 1]);
+  };
 
-    return asyncFunction()
-      .then((response) => {
-        setState({
-          result: response,
-          error: null,
-          status: 'settled',
-        });
-      })
-      .catch((err) => {
-        setState({
-          result: null,
-          error: err,
-          status: 'error',
-        });
-      });
-  }, [asyncFunction]);
 
-  useEffect(() => {
-    if (shouldRun) {
-      run();
-    }
-  }, [run, shouldRun]);
-
-  return [run, state.result, state.error, state.status];
-};
-
-const fetchData = async () => {
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts/');
-  const json = await data.json();
-  return json;
-};
-
- const Home = () => {
-  const [posts, setPosts] = useState(null);
-  const [reFetchData, result, error, status] = useAsync(fetchData, true);
-  const [reFetchData2, result2, error2, status2] = useAsync(fetchData, true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      reFetchData();
-    }, 6000);
-  }, [reFetchData]);
-
-  useEffect(() => {
-    console.log(result2);
-  }, [result2]);
-
-  function handleClick() {
-    reFetchData();
-  }
-
-  if (status === 'idle') {
-    return <pre>idle: Nada executando</pre>;
-  }
-
-  if (status === 'pending') {
-    return <pre>pending: Loading...</pre>;
-  }
-
-  if (status === 'error') {
-    return <pre>error: {error.message}</pre>;
-  }
-
-  if (status === 'settled') {
-    return <pre onClick={handleClick}>settled: {JSON.stringify(result, null, 2)}</pre>;
-  }
-
-  return 'IXIII';
-};
+  return(
+    <>
+      <button onClick={handleCLick}>Count: {counted.slice(-1)}</button>
+      <div ref={divRef} style={{height: '100px', width:'100px', overflowY: 'scroll' }}>
+        {counted.map((c) => {
+        return <p key={`c-${c}`}>{c}</p>
+      })}
+      </div>
+    </>
+  );
+}
 
 export default Home;

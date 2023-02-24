7f3a8332eb76d00491fb642a53bedc429288afbf
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-02-24 16:34:33 -0300

    useHistory e useLocation

--- ReactJsUd/project-5RouterDomV5/src/templates/Abc/index.jsx
+++ ReactJsUd/project-5RouterDomV5/src/templates/Abc/index.jsx
@@ -1,7 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useHistory, useLocation, useParams } from "react-router-dom";
 
 export const Abc = () => {
     const {slug, id} = useParams();
+    const location = useLocation();
+    const history = useHistory();
+
+    console.log(history);
+    console.log(location);
+
+    useEffect(() => {
+        setTimeout(() => {
+            history.push('/');
+        }, 5000);
+    }, [history]);
 
     return(
         <div>
--- ReactJsUd/project-5RouterDomV5/src/templates/App.jsx
+++ ReactJsUd/project-5RouterDomV5/src/templates/App.jsx
@@ -1,10 +1,21 @@
 
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Posts } from '../components/Posts';
 import { CounterProvider } from '../contexts/CounterProvider';
 import { PostsProvider } from '../contexts/PostsProvider';
 import './styles.css';
 
 export function App() {
+
+  const history = useHistory();
+
+  useEffect(() => {
+      setTimeout(() => {
+          history.push('/abc');
+      }, 5000);
+  }, [history]);
+
 
   return(
    <CounterProvider>

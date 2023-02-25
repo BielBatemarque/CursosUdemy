f8a67f2baa015e39f863f1c2c7eaeb0a050754e8
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-02-25 13:51:12 -0300

    react Router DOM v5

--- ReactJsUd/project-5RouterDomV5/src/components/Menu/index.jsx
+++ ReactJsUd/project-5RouterDomV5/src/components/Menu/index.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+//a -> causa um full reload, já o link não apartir do link pode reaproveitar coisas da página
 
 export const Menu = () => {
     return(
         <nav style={{ width: '100%', display: 'flex', justifyContent:'space-around', height: '60px', alignItems: 'center'}}>
            <Link to='/'>Home</Link> 
+           <a href="/">Home (A)</a>
            <Link to='/abc'>Abc</Link>
+           <a href="/abc">Abc (a)</a>
         </nav>
     );
 };

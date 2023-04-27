8b09875d142bdb3b16def31a075375ba0cfd4b52
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-04-27 00:40:30 -0300

    ajustes no projeto2

--- ReactJsUd/project-2/src/contexts/CounterProvider/reducer.js
+++ ReactJsUd/project-2/src/contexts/CounterProvider/reducer.js
@@ -4,11 +4,11 @@ export const reducer = (state, action) => {
   switch (action.type) {
     case types.INCREMENT_COUNTER: {
       return { ...state, counter: state.counter + 1 };
-    }
+    };
     case types.DECREMENT_COUNTER: {
       return { ...state, counter: state.counter - 1 };
-    }
-  }
+    };
+  };
 
   return { ...state };
 };

8e47c6686d45bb0b90bab74aa63b7c3903b8644c
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-05-10 23:25:37 -0300

    componente header

--- ReactType/src/components/MainHeader/index.tsx
+++ ReactType/src/components/MainHeader/index.tsx
@@ -1,9 +1,21 @@
-import { Container } from './styles';
+import { Container, Profile, Welcome, UserName } from './styles';
+import { useMemo } from 'react';
+import emojis from '../../utils/emojis';
 
 export const MainHeader: React.FC = () => {
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length);
+        return emojis[indice];
+    }, []);
+
     return(
         <Container>
             <h1>MainHeader</h1>
+
+            <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
+                <UserName>Gabriel Batemarque</UserName>
+            </Profile>
         </Container>
     );
 };
--- ReactType/src/components/MainHeader/styles.ts
+++ ReactType/src/components/MainHeader/styles.ts
@@ -4,4 +4,16 @@ export const Container = styled.div`
         grid-area: MH;
         background-color: ${props => props.theme.colors.secundary};
         color: ${props => props.theme.colors.white};
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 10px;
+        border-bottom: 1px solid ${props => props.theme.colors.gray};
 `;
+
+export const Profile = styled.div`
+       color: ${props => props.theme.colors.white};
+`;
+
+export const Welcome = styled.h3``;
+export const UserName = styled.span``;

8e8b1e12d030d7e63fbbb0313431d9a64f89911d
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-05-11 02:01:54 -0300

    useState no toggle

--- ReactType/src/components/Toggle/index.tsx
+++ ReactType/src/components/Toggle/index.tsx
@@ -1,12 +1,14 @@
 import { Container, ToggleLabel, ToggleSelectot } from './styles';
+import { useState} from 'react';
 
 export const Toggle: React.FC = () => {
+    const [checked, setCheckd] = useState<boolean>(true);
     return(
         <Container>
             <ToggleLabel>Light</ToggleLabel>
                 <ToggleSelectot  
-                checked 
-                onChange={() => console.log('mudou')} 
+                checked={checked}
+                onChange={() => setCheckd(s => !s)} 
                 uncheckedIcon={false} 
                 checkedIcon={false}
                 />
